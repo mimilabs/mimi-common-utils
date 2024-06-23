c63a64679c792235042d8d1543034f74ff4e2c0a
yubinmimi <yubinmimi>
2024-06-23 23:25:28 +0000

    change header pattern done

---
+++ ingestion_utils.py
@@ -0,0 +1,44 @@
+# Databricks notebook source
+!pip install openpyxl
+
+# COMMAND ----------
+
+import re
+from datetime import datetime
+from dateutil.parser import parse
+import pandas as pd
+from pathlib import Path
+import csv
+
+
+def split_camel(text):
+    pattern = re.compile(r'(?<!^)(?=[A-Z][a-z])')
+    return pattern.sub(' ', text)
+
+def reduce_spaces(text):
+    return ' '.join(text.split())
+
+def remove_special_chars(text):
+    return re.sub(r'[^a-zA-Z0-9 _]', '', text)
+
+def format_header_varname(varname):
+    varname0 = varname
+    if varname[0] == "#":
+        # if the first character is '#', if often means 'number'
+        varname0 = 'num_' + varname
+    varname1 = remove_special_chars(varname0)
+    varname2 = varname1.replace('_', ' ')
+    varname3 = split_camel(varname2)
+    varname4 = reduce_spaces(varname3)
+    varname5 = varname4.replace(' ', '_')
+    varname6 = varname5.lower()
+    
+    return varname6
+
+def change_header(header_org):
+    return [format_header_varname(column)
+            for column in header_org]
+
+# COMMAND ----------
+
+
